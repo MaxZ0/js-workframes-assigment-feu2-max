d2f17d889b39513c9eaa06c3856adcd1189b1010
Max <55975910+MaxZ0@users.noreply.github.com>
2021-03-06 22:09:20 +0100

    i think i am done?

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,7 +1,29 @@
 import React from "react";
+import "../../styles/contact.scss";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className="container">
+      <h3>Contact</h3>
+      <h4>Adress</h4>
+      <div className="flex">
+        <p>Street: </p>
+        <p>3782 Wilson Avenue</p>
+      </div>
+      <div className="flex">
+        <p>City:</p>
+        <p>Richardson</p>
+      </div>
+      <div className="flex">
+        <p>State:</p>
+        <p>TX</p>
+      </div>
+      <div className="flex">
+        <p>State Full:</p>
+        <p>Texas</p>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
